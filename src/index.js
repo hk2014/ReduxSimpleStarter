12ec22e657cf3cb33f41ef3569da1861c16d925e
shawnhr <shawnk0818@gmail.com>
2018-02-28 12:38:45 -0800

    First Commit. Worked on index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//create a new component. this component should produce some html
+import React from  'react';
+import ReactDOM from  'react-dom'
+const App = () => {
+	return <div>Hi!</div>;//this is jsx code to be transpiled(just like es6 browser can't interpret this )
+	//boiler plate babel and webpack 
+	//this code gets into DOM and renders to html
+	//babel translate this into something like " createElement("div", null, "hi" )
+}
+//tip: we can create self closing tag </APP> in jsx if there is nothing inside
+//take this component's generated html and put it on the page(in the DOM)
+ReactDOM.render(<App/>, document.querySelector('.container')) //can't pass in APP (class). should be <APP> (instance)
+//APP inside <> is component name which is class and jsx turns it into instance 
+//2nd argument should be target element in index.html to append this App component
+//document.querySelector('.container') means "go find that div container and try to render App into that div"
